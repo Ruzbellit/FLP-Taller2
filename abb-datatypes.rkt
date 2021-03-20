9262ebb12c9802b217e085f7da6b1d535c9c5a3e
Mici <villanuevapaezchristian77@gmail.com>
2021-03-20 13:09:17 -0500

    guardado correctamente de abb-datatypes

--- abb-datatypes.rkt
+++ abb-datatypes.rkt
@@ -35,20 +35,48 @@
   )
 
 (define number->bintree
-  (lambda (n)
-    (node n (empty-bintree) (empty-bintree))
+  (lambda (num)
+    (node num (empty-bintree) (empty-bintree))
+    )
+  )
+
+(define empty-bintree?
+  (lambda (tree)
+    (cases bintree tree
+      (empty-bintree () #t)
+      (else #f))
+    )
+  )
+
+(define at-leaf?
+  (lambda (tree)
+    (cases bintree tree
+      (empty-bintree () #f)
+      (node (int left right) (and (empty-bintree? left) (empty-bintree? right)))
+      )
+    )
+  )
+
+(define bintree-with-at-least-one-child
+(lambda (tree)
+    (cases bintree tree
+      (empty-bintree () #f)
+      (node (int left right) (or (empty-bintree? left) (empty-bintree? right)))
+      )
     )
   )
 
 (define insert-to-left
   (lambda (num tree)
-    (node (current-element tree) (number->bintree n) ())))
+    (node (current-element tree) (number->bintree num) (empty-bintree))
+    )
+  )
 
 (define insert-to-right
-  (lambda (num tree)))
-
-(define bintree-order-validation
-  (lambda (tree)))
+  (lambda (num tree)
+    (node (current-element tree) (empty-bintree) (number->bintree num))
+    )
+  )
 
 (define arbol (node 5
                     (node 4 (empty-bintree) (empty-bintree))
