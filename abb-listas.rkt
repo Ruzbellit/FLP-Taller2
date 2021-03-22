b77d6c189c8b8367df61e8ed53ee12e10da53612
Mici <villanuevapaezchristian77@gmail.com>
2021-03-22 17:28:10 -0500

    añadido funciones parciales del arbol de busqueda por listas

--- abb-listas.rkt
+++ abb-listas.rkt
@@ -31,3 +31,37 @@
     (eqv? empty-bintree tree)
     )
   )
+
+(define at-leaf?
+  (lambda (tree)
+    (and (empty-bintree? (move-to-left-son)) (empty-bintree? (move-to-right-son)))
+    )
+  )
+
+(define bintree-with-at-least-one-child?
+  (lambda (tree)
+    (or (not (empty-bintree? (move-to-left-son))) (not (empty-bintree? (move-to-right-son))))
+    )
+  )
+
+(define insert-to-left
+  (lambda (num tree)
+    (cond
+      ))
+  )
+
+(define insert-to-right
+  (lambda (num tree)
+    (cond
+      ))
+  )
+
+(define bintree-order-validation
+  (lambda (tree)1
+    )
+  )
+
+(define insert-element-into-bintree
+  (lambda (tree num)1
+    )
+  )
