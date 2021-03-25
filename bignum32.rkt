dce87291783840cc5aa32b3dc7a5f5522fbcb0a6
Ruzbellit <ruzbellit.romero@correounivalle.edu.co>
2021-03-24 22:35:18 -0500

    Agrego punto 6 completito, prefix.jpg y prefix.rkt y comentarios a bignum32

--- bignum32.rkt
+++ bignum32.rkt
@@ -1,42 +1,94 @@
 #lang eopl
+;;Estudiantes:
+;;Ruzbellit Rossy Romero Ramirez (1925456)
+;;ruzbellit.romero@correounivalle.edu.co
+;;Christian Villanueva Paez (1924546)
+;;christian.villanueva@correounivalle.edu.co
 
+
+;; suma : BigNum32 BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el resultado de una suma 
 (define suma
   (lambda (x y)
     (if (is-zero? x)
         y
         (successor (suma (predecessor x) y)))))
+;; Pruebas
+;(suma '(0 1) '(1))   //'(1 1)
+;(suma '(31) '(2))    //'(1 1)
+;(suma '(31) '(1))    //'(0 1)
+;(suma '(31) '(31))   //'(30 1)
+;------------------------------------------------------------------------------
 
+;; resta : BigNum32 BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el resultado de una resta 
 (define resta
   (lambda (x y)
     (if (is-zero? y)
         x
         (predecessor (resta  x (predecessor y))))))
+;; Pruebas
+;(resta '(0 2) '(1))   //'(31 1)
+;(resta '(0 1) '(1))   //'(31)
+;(resta '(1 1) '(0))   //'(1 1)
+;------------------------------------------------------------------------------
 
+;; multiplicacion : BigNum32 BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el resultado de una multiplicacion 
 (define multiplicacion
   (lambda (x y)
     (if (is-zero? x)
         (zero)
-        (suma (multiplicacion (predecessor x) y) y))
-    ))
-    
+        (suma (multiplicacion (predecessor x) y) y))))
+;; Pruebas
+;(multiplicacion '(31) '(2))     // '(30 1)  
+;(multiplicacion '(0 1) '(1))    // '(0 1)
+;(multiplicacion '(1 2) '(0))    // '(0)
+;(multiplicacion '(31) '(31))    // '(1 30)
+;------------------------------------------------------------------------------
+
+;; potencia : BigNum32 BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el resultado de una potencia
 (define potencia
   (lambda (x y)
     (if (is-zero? y)
         (successor y)
         (multiplicacion (potencia x (predecessor y)) x))))
+;; Pruebas
+;(potencia '(31) '(2))     // '(1 30)  
+;(potencia '(1 2) '(1))    // '(1 2)
+;(potencia '(31 1) '(0))   // '(1)
+;------------------------------------------------------------------------------
 
+;; factorial : BigNum32 BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el resultado de una factorial
 (define factorial
   (lambda (n)
     (if (is-zero? n)
         (successor n)
         (multiplicacion n (factorial (predecessor n))))))
+;; Pruebas
+;(factorial '(2))     // '(2)  
+;(factorial '(0))     // '(1)
+;(factorial '(5))     // '(24 3) 
+;------------------------------------------------------------------------------
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
 
+;;Representacion Bignum con base 32
 (define zero (lambda () '(0)))
 
 (define is-zero? (lambda (n) (equal? n '(0))))
 
+;; successor : BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el sucesor de un numero
 (define successor (lambda (n)
                     (if (null? n)
                         '(1)
@@ -44,6 +96,9 @@
                             (cons (+ (car n) 1) (cdr n))
                             (cons 0 (successor (cdr n)))))))
 
+;; predecessor : BigNum32 -> BigNum32
+;; Proposito:
+;; Procedimiento que retorna el predecessor de un numero 
 (define predecessor (lambda (n)
                     (cond
                       [(null? n) '(0)]
@@ -67,9 +122,9 @@
 ;; '(0 0 1)
 ;; Ejemplos de prueba predecessor
 (predecessor '(1))
-;; '(1)
+;; '(0)
 (predecessor '(31))
-;; '(0 1)
+;; '(30)
 (predecessor '(0 1))
 ;; '(31)
 (predecessor '(0 2))
