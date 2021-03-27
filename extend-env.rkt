54e33264a394c8fa2bedc3ec1b1b70b989ce1479
Ruzbellit <ruzbellit.romero@correounivalle.edu.co>
2021-03-26 21:37:48 -0500

    Adicionan comentarios finales

--- extend-env.rkt
+++ extend-env.rkt
@@ -1,8 +1,10 @@
 #lang eopl
 
 ;; Gramática
-;; Env = (empty-env) | (extend-env Var SchemeVal Env)
-;; Var = Sym
+;; Env := (empty-env)
+;;     := (extend-env Var SchemeVal Env)
+
+;; Var := Symbol
 
 ;empty-env : () → Env
 (define empty-env
@@ -38,14 +40,31 @@
     (eopl:error 'apply-env "Bad environment: ~s" env)))
 
 
-;; Gramática extendida (con extend-env*)
+;; Gramática actualizada con extend-env*
 ;; Env := (empty-env)
 ;;     := (extend-env Var SchemeVal Env)
 ;;     := (extend-env* (Var SchemeVal)* Env)
 ;; Var := Symbol
 
-;extend-env*
-;extiende cada variable asociada con cada valor en el ambiente dado.
+
+;; extend-env*-aux; list x lista -> List of Pairs
+;; Proposito:
+;; funcion auxiliar de extend-env* que asocia una lista de variables con sus valores
+(define extend-env*-aux
+  (lambda (vars vals)
+    (if (null? vars)
+        '()
+        (cons (cons (car vars) (car vals)) (extend-env*-aux (cdr vars) (cdr vals))))))
+;;Pruebas
+(extend-env*-aux '(x y z) '(1 2 3))
+;((x . 1) (y . 2) (z . 3))
+(extend-env*-aux '(a b c) '(0 9 8))
+;((a . 0) (b . 9) (c . 8))
+
+
+;; extend-env* : List x List x env -> List
+;; Proposito:
+;; extiende cada variable asociada con cada valor en el ambiente dado.
 (define extend-env*
   (lambda (vars vals env)
     (if (null? vars)
@@ -53,17 +72,12 @@
         (list 'extend-env*
               (extend-env*-aux vars vals)
               env))))
+;;Pruebas
+(extend-env* '(x y z) '(2 3 4) (empty-env) )
+;(extend-env* ((x . 2) (y . 3) (z . 4)) (empty-env))
+(extend-env* '(R u Z) '(6 6 6) (extend-env 'r 17 (empty-env)) )
+;(extend-env* ((R . 6) (u . 6) (Z . 6)) (extend-env r 17 (empty-env)))
 
-;extend-env*-aux; list x lista -> List of Pairs
-; funcion auxiliar de extend-env* que asocia una lista de variables con sus valores
-(define extend-env*-aux
-  (lambda (vars vals)
-    (if (null? vars)
-        '()
-        (cons (cons (car vars) (car vals)) (extend-env*-aux (cdr vars) (cdr vals))))))
-;Pruebas
-(extend-env*-aux '(x y z) '(1 2 3))
-;((x . 1) (y . 2) (z . 3))
 
 ; Ambientes de prueba
 (define env1 (extend-env 'r 17 (empty-env)))
@@ -75,27 +89,43 @@
                            (extend-env 'a 7
                                        (empty-env)))))
 
-;depth-env: env -> Int
-;Retorna la profundidad de un ambiente
+
+; depth-env: env -> Int
+; Proposito:
+; Retorna la profundidad de un ambiente
 (define depth-env
   (lambda (env)
     (cond
      [(eqv? (car env) 'empty-env) 0]
      [(eqv? (car env) 'extend-env) (+ 1 (depth-env (cadddr env)))]
      [(eqv? (car env) 'extend-env*) (+ 1 (depth-env (caddr env)))])))
-;Pruebas
+;;Pruebas
 (depth-env e)
 ; 3
+(depth-env env3)
+;2
 
-;get-var-values-env: env -> List
-;Retorna las variables con sus valores correspondientes de un ambiente dado
+
+; get-var-values-env: env -> List
+; Proposito:
+; Retorna las variables con sus valores correspondientes de un ambiente dado
 (define get-var-values-env
   (lambda (env)
     (cond
       [(eqv? (car env) 'empty-env) '()]
       [(eqv? (car env) 'extend-env) (list (cons (cadr env) (caddr env)))]
       [(eqv? (car env) 'extend-env*) (list (cadr env))])))
+;;Pruebas
+(get-var-values-env env1)
+; ((r . 17))
+(get-var-values-env (empty-env))
+;()
 
+
+; check-env: env x Int -> List
+; Proposito:
+; retorna una lista con 1 o mas parejas de valores asociados que
+; pertenecen al n- ́esimo nivel del ambiente.
 (define check-env
   (lambda (e n)
     (if (<= n (depth-env e))
@@ -106,5 +136,15 @@
               [(eqv? (car e) 'extend-env) (check-env (cadddr e) n)]
               [(eqv? (car e) 'extend-env*) (check-env (caddr e) n)]))
         (eopl:error 'check-env "Not possible to search depth on environment"))))
-
+;;Pruebas
+(check-env e 0)
+;()
+(check-env e 1)
+;((a 7))
+(check-env e 2)
+;((x 1) (z 4) (w 5))
+(check-env e 3)
+;((y . 8))
+;;(check-env e 4)
+;check-env: Not possible to search depth on environment
 
