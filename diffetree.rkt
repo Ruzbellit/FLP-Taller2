34c194185974b1070ba45e81adcb2f814eb4e67b
Mici <villanuevapaezchristian77@gmail.com>
2021-03-26 23:37:17 -0500

    solucion y documentado de difftree

--- diffetree.rkt
+++ diffetree.rkt
@@ -1,50 +1,75 @@
-;;Ruzbellit Rossy Romero Ramirez - 1925456
-;;Christian Villanueva Paez - 1924546
-
 #lang eopl
+;;Estudiantes:
+;;Ruzbellit Rossy Romero Ramirez (1925456)
+;;ruzbellit.romero@correounivalle.edu.co
+;;Christian Villanueva Paez (1924546)
+;;christian.villanueva@correounivalle.edu.co
 
-
-;;******************************************************************************************
-
-;; Gramatica para diff-tree
+;; GRAMATICA PARA DIFF-TREE
 ;;
 ;; <Diff-tree>    ::= (one)
 ;:                ::= (diff <Diff-tree> <Diff-tree>)
-;******************************************************************************************
+;;-----------------------------------------------------------------------------------
 
-(define one
-  (lambda ()
-    (list 'one))
-  )
+;; Diff-tree->Number : Diff-tree -> Number
+;; Proposito:
+;; Procedimiento que transforma una representacion de Diff-tree en un numero
+(define Diff-tree->Number
+  (lambda (diff-tree)
+    (if (eqv? (car diff-tree) 'one)
+        1
+        (- (Diff-tree->Number (cadr diff-tree))
+           (Diff-tree->Number (caddr diff-tree))))))
+;;-----------------------------------------------------------------------------------
 
-
-(define Diff-tree
-  (lambda (diff-tree-l diff-tree-r)
-    (list 'diff diff-tree-l diff-tree-r)
-     )
-  )
-
+;; zero :  -> Diff-tree
+;; Proposito:
+;;Procedimiento que retorna una representacion del numero 0 para Diff-tree
 (define zero
-  (lambda () '()
+  (lambda ()
+    '(diff (one) (one))
     )
   )
+;;-----------------------------------------------------------------------------------
 
-(define iszero?
-  (lambda (n)
-    (eqv? n 0))
+;; is-zero? : Diff-tree -> Boolean
+;; Proposito:
+;; Procedimiento que verifica si una representacion del Diff-tree
+;; es igual al numero 0
+(define is-zero?
+  (lambda (diff-tree)
+    (eqv? (Diff-tree->Number (zero))
+          (Diff-tree->Number diff-tree)
+          )
+    )
   )
+;;-----------------------------------------------------------------------------------
 
+;; successor : Diff-tree -> Diff-tree
+;; Proposito:
+;; Procedimiento que retorna el numero posterior de la representacion de Diff-tree
 (define successor
   (lambda (diff-tree)
-    '()
+    (list 'diff diff-tree '(diff (diff (one) (one)) (one)))
      )
   )
+;;-----------------------------------------------------------------------------------
 
+;; predecessor : Diff-tree -> Diff-tree
+;; Proposito:
+;; Procedimiento que retorna el numero anterior de la representacion de Diff-tree
 (define predecessor
-  (lambda (n)1))
+  (lambda (diff-tree)
+    (list 'diff diff-tree '(one)))
+  )
+;;-----------------------------------------------------------------------------------
 
+;; diff-tree-plus : Diff-tree x Diff-tree -> Diff-tree
+;; Proposito:
+;; Procedimiento que suma las representaciones de Diff-tree
 (define diff-tree-plus
-  (lambda (tree1 tree2)
-    5
+  (lambda (dif-tree1 dif-tree2)
+     (list 'diff dif-tree1 (list 'diff '(diff (one) (one)) dif-tree2))
     )
   )
+;;-----------------------------------------------------------------------------------
